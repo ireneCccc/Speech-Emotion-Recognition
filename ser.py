4caf9b89f184e67784edc9c0a3061ef72b179b22
unt <tw1701@nyu.edu>
2018-05-03 17:40:27 -0400

    complete find_nearest()

--- ser.py
+++ ser.py
@@ -5,13 +5,17 @@ def audioread(m, filename):
         fs = 24414
 
 def spectrogram(x, win_size = 1024, hop_size = 512):
-    return m.spectrogram(x, m.hamming(win_size), win_size - hop_size)
+    return m.s(x, m.hamming(win_size), win_size - hop_size)
 
-def find_nearest(m, values, ref):
-    #diff = 
-    #diff = m.repmat(m.transpose(ref), 1, len(values)) - m.repmat(values, len(ref), 1)
-    #refs, index = m.min(m.abs(diff))
-    return refs, index
+def find_nearest(value, ref):
+    if value <= ref[0]:
+        return 0
+    for i in range(0, len(ref) - 1):
+        if ref[i] <= value and value <= ref[i + 1]:
+            if value - ref[i] <= ref[i + 1] - value:
+                return i
+            return i + 1
+    return len(ref) - 1 # value > ref[-1]
 
 def hz2mel(hz):
     return 1127.01028 * m.log(1 + hz / 700)
@@ -19,7 +23,7 @@ def hz2mel(hz):
 def mel2hz(mel):
     return (m.exp(mel / 1127.01028) - 1) * 700
 
-def mfcc(spectrogram, min_hz = 80, max_hz = 8000, n_mel = 40, n_dct = 15):
+def mfcc(s, f, min_hz = 80, max_hz = 8000, n_mel = 40, n_dct = 15):
     # fundamental frequency of adult male voice is 85 ~ 180 Hz
     # fundamental frequency of adult female is 165 ~ 255 Hz
     # frequency range of adult male voice is upto 8 kHz
@@ -34,4 +38,5 @@ def mfcc(spectrogram, min_hz = 80, max_hz = 8000, n_mel = 40, n_dct = 15):
     # create the filterbank matrix
     # number of rows: number of filters
     # number of columns: number of frequency bins in spectrogram, i.e. length of f
-    
+    #for i in range(0, len(centers)):
+    #    centers[i] = find_nearest(centers[i], f)
