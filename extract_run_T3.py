eca2e57567daa2c4429a66af72a43cb6c2c8261b
unt <tw1701@nyu.edu>
2018-05-11 12:04:15 -0400

    modify caller of extract.py (using MATLAB engine)

--- extract_run_T3.py
+++
@@ -1,4 +0,0 @@
-import os, matlab.engine, extract
-m = matlab.engine.start_matlab()
-parent_dir = os.path.dirname(os.getcwd())
-extract.run(parent_dir, m, [1024, 512, 24414, 80, 300, 8000], 7, -1, 'T3')
